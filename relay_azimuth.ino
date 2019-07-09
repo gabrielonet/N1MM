c851ba95e3a51a56ee3ccd363d442d014132bc3d
gabrielonet <gabriel@linux-romania.com>
2019-07-09 20:40:03 +0300

    Update relay_azimuth.ino

--- relay_azimuth.ino
+++ relay_azimuth.ino
@@ -6,8 +6,9 @@ int se ;
 int sw ;
 int temp = 0;
 int value = 0;
-char angle[20];
+char angle[4]  = {"300"};
 String incomingByte ;   // for incoming serial data
+int azimuth =300  ;
 
 
 void setup(void) {
@@ -17,14 +18,14 @@ void setup(void) {
   tft.setRotation(1);
   tft.fillScreen(TFT_BLACK);
   tft.drawLine(240, 160, 0, 0, TFT_GREEN);
-  pinMode(0, OUTPUT) ; //   just to disable TX0
-  digitalWrite (0, HIGH);  
-  pinMode(1, OUTPUT) ; //   just to disable TX0
-  digitalWrite (1, HIGH);  
+  pinMode(8, OUTPUT) ; //   just to disable TX0
+  digitalWrite (8, HIGH);  
+  pinMode(9, OUTPUT) ; //   just to disable TX0
+  digitalWrite (9, HIGH);  
   pinMode(2, OUTPUT) ; //   just to disable TX0
   digitalWrite (2, HIGH);  
   pinMode(3, OUTPUT) ; //   just to disable TX0
-  digitalWrite (3, HIGH);
+  digitalWrite(3, HIGH);   
   background();
   //tft.drawLine(180, 160, 100, 50, TFT_GREEN);
   //tft.fillTriangle(73, 34, 10, 63, 73, 93, TFT_RED);
@@ -43,8 +44,14 @@ void setup(void) {
 }
 void background(){
   tft.fillScreen(TFT_BLACK);  tft.setTextColor(TFT_GREEN);
+  tft.drawCentreString(angle, 220,230, 4);  
+  tft.drawCentreString("Deg", 270,230, 4);
+  tft.drawCentreString("SP Heading", 240,270, 4);  
+
+
   tft.drawRect(0, 0, 100, 50, TFT_GREEN);
   tft.drawCentreString("VE", 50,15, 4);
+  
   tft.drawRect(380, 0, 100, 50, TFT_GREEN);
   tft.drawCentreString("JA", 430,15, 4);
   
@@ -56,26 +63,13 @@ void background(){
 
  tft.drawCircle(240,160, 60, TFT_YELLOW);
  tft.drawCentreString("YO8RXP", 240,150, 4);
-
-
-
-
- 
-
-    
   }
-
-
 
 void north_west() {
    background();
    tft.drawLine(180, 160, 100, 50, TFT_GREEN);
   digitalWrite(4, LOW);  digitalWrite(5, LOW);
   digitalWrite(6, LOW);  digitalWrite(7, LOW);
-
-
-   
-
 }
 void north_east() {
    background();
@@ -102,26 +96,29 @@ void loop() {
                 
                 // read the incoming byte:
                 incomingByte = Serial.readString();
-                incomingByte.toCharArray(angle,20);
-                background();
-                tft.drawCentreString(angle+2, 240,250, 4);
+                incomingByte.toCharArray(angle ,20);
+                azimuth = atoi(angle);
+                             
   }
 
-  int azimuth = atoi(angle);
   
-  //nw = digitalRead(0); 
-  //ne = digitalRead(3); 
-  //se = digitalRead(2); 
-  //sw = digitalRead(1); 
+  
+  nw = digitalRead(8); 
+  ne = digitalRead(3); 
+  se = digitalRead(2); 
+  sw = digitalRead(9); 
+  
+  if (nw != 1 && temp != 1){azimuth = 315 ; itoa(azimuth, angle, 10);north_west(); temp = 1 ;}
+  if (ne != 1 && temp != 2){azimuth = 45 ;itoa(azimuth, angle, 10);north_east(); temp = 2 ;  }
+  if (se != 1 && temp != 3){azimuth = 135 ;itoa(azimuth, angle, 10); south_east(); temp = 3 ; }
+  if (sw != 1 && temp != 4){azimuth = 225 ; itoa(azimuth, angle, 10);south_west(); temp = 4 ;}
 
-  if ( azimuth >= 270 && azimuth  <= 359 && temp != 1  ){ north_west(); temp = 1; }
+  if ( azimuth >= 270 && azimuth  <= 359 && temp != 1  ){north_west(); temp = 1; }
   if ( azimuth >= 0   && azimuth  <= 90  && temp != 2  ){ north_east(); temp = 2; }
   if ( azimuth >= 91 && azimuth  <= 180 && temp != 3  ){ south_east(); temp = 3; }
   if ( azimuth >= 181 && azimuth  <= 269 && temp != 4  ){ south_west(); temp = 4; }
 
-  //if (ne != 1 && temp != 2){ north_east(); temp = 2; }
-  //if (se != 1 && temp != 3){ south_east(); temp = 3; }
-  //if (sw != 1 && temp != 4){ south_west(); temp = 4; }
+
+  
   
 }
-
